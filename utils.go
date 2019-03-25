90355a9491fab5c476147663060217136d0e7c35
AndyDong <andydong@cybavo.com>
2019-03-25 16:51:31 +0800

    Add Copyright

--- utils.go
+++ utils.go
@@ -1,3 +1,13 @@
+// Copyright (c) 2018-2019 The Cybavo developers
+// All Rights Reserved.
+// NOTICE: All information contained herein is, and remains
+// the property of Cybavo and its suppliers,
+// if any. The intellectual and technical concepts contained
+// herein are proprietary to Cybavo
+// Dissemination of this information or reproduction of this materia
+// is strictly forbidden unless prior written permission is obtained
+// from Cybavo.
+
 package main
 
 import (
